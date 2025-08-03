943832c742f32abae267bacd8b74278c760e8ebc
Gabriel Cardoso de Castro <gabrielcdc13@gmail.com>
2025-08-03 03:25:01 -0300

    get sensor data and display on leds

--- EnvironmentalMonitor/src/main.cpp
+++ EnvironmentalMonitor/src/main.cpp
@@ -1,22 +1,63 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h> 
 
 const int lightPinDetector = 2;
 const int lightPin = 3;
+const int DHTPin = 4;
+
+const int humidityLedPin = 5;
+const int temperatureLedPin = 6;
+
+const int redPinLed = 9;
+const int greenPinLed = 10;
+const int bluePinLed = 11;
+
+#define DHTTYPE DHT22
+DHT dht(DHTPin, DHTTYPE);  
+
+void PrintValues(bool hasNoLight, float humidity, float temperature);
+void SetLedOnIfNoLight(bool hasNoLight);
+void DisplayHumidityLevel(float humidity);
+void DisplayTemperatureLevel(float temperature);
 
 void setup() {
   Serial.begin(9600);
+
+  dht.begin();
   
   pinMode(lightPinDetector, INPUT);
   pinMode(lightPin, OUTPUT);
+  pinMode(redPinLed, OUTPUT);
+  pinMode(greenPinLed, OUTPUT);
+  pinMode(bluePinLed, OUTPUT);
+  pinMode(humidityLedPin, OUTPUT);
+  pinMode(temperatureLedPin, OUTPUT);
 }
 
 void loop() {
   bool hasNoLight = digitalRead(lightPinDetector);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-  Serial.print("light detected: ");
-  Serial.print(!hasNoLight);
-  Serial.println("");
+  //Start procedures:
+  PrintValues(hasNoLight, humidity, temperature);
+  SetLedOnIfNoLight(hasNoLight);
+  DisplayHumidityLevel(humidity);
+  DisplayTemperatureLevel(temperature);
+}
 
+void PrintValues(bool hasNoLight, float humidity, float temperature){
+  Serial.print("Light detected: ");
+  Serial.println(!hasNoLight ? "Yes" : "No");
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  Serial.println("\n------------------------");
+}
+
+void SetLedOnIfNoLight(bool hasNoLight){
   if (hasNoLight)
   {
     digitalWrite(lightPin, HIGH);
@@ -24,6 +65,36 @@ void loop() {
   {
     digitalWrite(lightPin, LOW);
   }
+}
 
-  delay(100);
+void DisplayHumidityLevel(float humidity){
+
+  int humidityValue = map(humidity, 0, 100, 0, 255);
+
+  digitalWrite(humidityLedPin, HIGH);
+  analogWrite(bluePinLed, humidityValue);
+  
+  delay(1000);
+  
+  analogWrite(bluePinLed, 0);
+  digitalWrite(humidityLedPin, LOW);
+}
+
+void DisplayTemperatureLevel(float temperature){
+
+  if (temperature < 10) temperature = 10;
+  if (temperature > 40) temperature = 40;
+
+  int pwmValue = map(temperature, 10, 40, 0, 255);
+  pwmValue = constrain(pwmValue, 0, 255);
+
+  int temperatureValue = 255 - pwmValue;
+
+  digitalWrite(temperatureLedPin, HIGH);
+  analogWrite(greenPinLed, temperatureValue);
+
+  delay(1000);
+
+  analogWrite(greenPinLed, 0);
+  digitalWrite(temperatureLedPin, LOW);
 }
